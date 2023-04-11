3a46c07dda9853ac2a52f5f16f73514e469e8c7c
DominikKnapczyk <dominik.kna.01@gmail.com>
2023-04-11 16:09:56 +0200

    + show()

--- resources/views/musics/index.blade.php
+++ resources/views/musics/index.blade.php
@@ -1,13 +1,32 @@
 @extends('layouts.app')
 
-@section('navbar')
-  navbar
-@endsection
+@section('page-name', 'Lista delle canzoni')
 
 @section('main-content')
-  Lista delle canoni
-@endsection
-
-@section('footer')
-  footer
+  <table class="table table-dark table-striped">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Titolo</th>
+        <th scope="col">Album</th>
+        <th scope="col">Autore</th>
+        <th scope="col">Editore</th>
+        <th scope="col">Lunghezza</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($musics as $music)    
+        <tr>
+          <th scope="row">{{ $music->id}}</th>
+          <td>{{ $music->title}}</td>
+          <td>{{ $music->album }}</td>
+          <td>{{ $music->author }}</td>
+          <td>{{ $music->editor }}</td>
+          <td>{{ $music->length }} min</td>
+          <td><a href="{{ route('musics.show', ['music' => $music]) }}">Dettaglio</a></td> 
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 @endsection
